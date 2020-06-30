93031c221e4cc3270cfcd4e92fb0b88fccde010d
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2020-06-30 14:59:33 -0400

    Fixing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_cors import CORS
-from models import db
+from models import db, Queue
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,10 +18,25 @@ manager.add_command("db", MigrateCommand)
 CORS(app)
 
 
+queue = Queue()
+
 @app.route('/')
 def main():
     return render_template('index.html')
 
+
+@app.route('/new', methods=['POST'])
+def new_item():
+    pass
+
+@app.route('/next', methods=['GET'])
+def next_item():
+    pass
+
+
+@app.route('/all', methods=['GET'])
+def all_items():
+    pass
 
 if __name__ == '__main__':
     manager.run()
--- models.py
+++ models.py
@@ -1,18 +1,40 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from twilio.rest import Client
 db = SQLAlchemy()
+
 
 class Queue:
 
     def __init__(self):
+        # CONFIG TWILIO
+        self.account_sid = os.environ.get('ACCOUNT_ID')
+        self.auth_token = os.environ.get('AUTH_TOKEN')
+        self.client = Client(self.account_sid, self.auth_token)
+
         self._queue = []
         # depending on the _mode, the queue has to behave like a FIFO or LIFO
         self._mode = 'FIFO'
 
     def enqueue(self, item):
+        """ message = self.client.messages \
+            .create(
+                body='This is the ship that made the Kessel Run in fourteen parsecs?',
+                from_= os.environ.get('PHONE'),
+                to='+5699999999'
+            )
+        print(message.sid) """
         pass
+
     def dequeue(self):
         pass
+
     def get_queue(self):
         pass
+
     def size(self):
         return len(self._queue)
+
+
+
+
